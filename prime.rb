1f2fe2a6a5c2d336c677c841e0f8f534f68189c6
michaelmirzakandov <mtrix92@gmail.com>
2022-05-19 14:20:09 -0400

    commit tree bc142feafb2677308404c8d3d8f1b87ffdfe05c1

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+def prime?(num)
+    if num == 1
+        return false
+    end
+    if num <= 0
+        return false
+    end
+    i = num -1 
+    while i > 1
+        if num % i == 0
+            return false
+        else
+            i-=1
+        end
+    end
+    true
+
+end
